f5b5715222b2c1967b94b97ef5f390d6500b5f8c
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-11 12:51:57 -0700

    updated

--- src/components/APNavbar.jsx
+++ src/components/APNavbar.jsx
@@ -1,10 +1,18 @@
 import APLogo from "../assets/images/ApcBlack.PNG"
 
-const APNavbar = () =>{
-    return(
-        <div>
-            <h1>Apple Pie Navbar</h1>
-            <img src={APLogo} className="w-[200px]"/>
+const APNavbar = () => {
+    return (
+        <div className="w-full bg-slate-200 h-[270px] flex items-center justify-between">
+            <div>
+                <img src={APLogo} className="w-[200px] ml-[60px]" />
+            </div>
+
+            <div className="flex justify-evenly w-[500px]">
+                <h1>Home</h1>
+                <h1>Menu</h1>
+                <h1>About</h1>
+            </div>
+
         </div>
     )
 }
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -1,8 +1,11 @@
-
+import applePie from "../assets/images/apphoto.jpeg"
 
 const LandingPage = () =>{
     return(
         <div>
+            <div className="w-full h-[500px]">
+                <img src={applePie} className="w-full h-[500px] object-cover"/>
+            </div>
             <h1>landing page restuarant</h1>
         </div>
     )
