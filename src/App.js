07c4a8eac786d14baf499c572cd5705ff7e05b1a
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-14 12:02:09 -0700

    updated

--- src/App.js
+++ src/App.js
@@ -4,25 +4,32 @@ import LandingPage from './pages/landingPage';
 import APNavbar from './components/APNavbar';
 import MenuPage from './pages/menuPage';
 import AdminPage from './pages/adminPage';
+import AdminLandingView from './views/adminLandingView';
 
 function App() {
 
   const currentUser = false;
-  
-  const RequireAuth = ({children}) => {
-    return currentUser ? (children) : <Navigate to="/admin"/>
+
+  const RequireAuth = ({ children }) => {
+    return currentUser ? (children) : <Navigate to="/admin" />
   }
 
   return (
     <div className="App">
 
-    <APNavbar/>
-    
-    <Routes>
-      <Route path="/" element={<LandingPage/>}/>
-      <Route path="/menu" element={<MenuPage/>}/>
-      <Route path="/admin" element={<AdminPage/>}/>
-    </Routes>
+      <APNavbar />
+
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        <Route path="/admin" element={<AdminPage />} />
+
+        {/* Private Routes */}
+        <Route path="/adminLp" element={<RequireAuth><AdminLandingView /></RequireAuth>} />
+
+        
+      </Routes>
     </div>
   );
 }
