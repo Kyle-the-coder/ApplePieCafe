5c839cd87da37f3aa82100c2dabf3c7a08f99c08
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-14 12:27:45 -0700

    added Auth to localStorage for persistent Login

--- src/views/adminLandingView.jsx
+++ src/views/adminLandingView.jsx
@@ -4,6 +4,7 @@ const AdminLandingView = () => {
     return(
         <div>
             <h1>welcome to admin page</h1>
+            <h1 className="underline text-sky-600">logout</h1>
         </div>
     )
 }
