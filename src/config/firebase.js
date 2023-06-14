53ce38396096da99ec47f3eb2ad7d670a4ca2666
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-14 11:13:52 -0700

    updated

---
+++ src/config/firebase.js
@@ -0,0 +1,17 @@
+
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: "restuarantsite.firebaseapp.com",
+    projectId: "restuarantsite",
+    storageBucket: "restuarantsite.appspot.com",
+    messagingSenderId: "1006709987339",
+    appId: "1:1006709987339:web:084dd0988794f1747ccf0e",
+    measurementId: "G-9S4RY7Y9DT"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
