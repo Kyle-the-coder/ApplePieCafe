51599d0f25ce28d991cfe3c3b4107be057e62872
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-14 10:37:50 -0700

    updated

---
+++ src/components/reviewModal.jsx
@@ -0,0 +1,97 @@
+import avatar from "../assets/images/avatar.png"
+import wAvatar from "../assets/images/woman.png"
+import starBlank from "../assets/images/starBlank.png"
+import starFill from "../assets/images/starFill.png"
+
+const ReviewModal = () => {
+    return (
+        <div>
+            <div>
+                <h1 className="fontWriting text-4xl">Few Reviews:</h1>
+            </div>
+
+            <div className="flex w-full h-600px justify-center">
+
+                {/* Review Card */}
+                <div className="flex flex-col items-center w-[300px] h-[350px] bg-blue-200 border rounded border-[3px] border-black m-4">
+                    <div className="w-11/12  h-1/2 mt-3">
+                        <div className="flex items-center">
+                            <img src={avatar} width="64px" />
+                            <h1 className="ml-3">Scott Miguel</h1>
+                        </div>
+                        <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
+                            <p className="bg-white p-2">"I recently had the pleasure of visiting Apple Pie Cafe, and it was an absolute delight. From the moment I stepped in, I was greeted with the warm and inviting aroma of freshly baked pies. The cozy atmosphere, coupled with the friendly and attentive staff, made me feel right at home. The apple pies were simply divine, with flaky crusts that melted in my mouth and fillings bursting with the perfect balance of sweetness and spice. The menu offered a wide variety of flavors and even some unique twists that were a pleasant surprise. The care and attention to detail put into each pie were evident, and it was clear that they were made with love. I left with a happy heart and a newfound appreciation for the art of pie-making. Apple Pie Cafe is a must-visit for any pie lover or anyone looking to experience a slice of Americana in the most delicious way possible."</p>
+                        </div>
+                    </div>
+                    <div className="flex mt-16">
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                    </div>
+                </div>
+
+                {/* Review Card */}
+                <div className="flex flex-col items-center w-[300px] h-[350px] bg-pink-200 border rounded border-[3px] border-black m-4">
+                    <div className="w-11/12  h-1/2 mt-3">
+                        <div className="flex items-center mb-2">
+                            <img src={wAvatar} width="58px" />
+                            <h1 className="ml-3">Olivia Davis</h1>
+                        </div>
+                        <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
+                            <p className="bg-white p-2">"The coffee at Apple Pie Cafe is a delightful complement to their mouthwatering pies. Served piping hot and expertly brewed, it provides a rich and flavorful experience. Whether enjoyed alongside a slice of warm apple pie or on its own, the coffee at Apple Pie Cafe is the perfect way to start your day or unwind in a cozy atmosphere."</p>
+                        </div>
+                    </div>
+                    <div className="flex mt-16">
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                    </div>
+                </div>
+                {/* Review Card */}
+                <div className="flex flex-col items-center w-[300px] h-[350px] bg-green-200 border rounded border-[3px] border-black m-4">
+                    <div className="w-11/12  h-1/2 mt-3">
+                        <div className="flex items-center mb-2">
+                            <img src={wAvatar} width="58px " />
+                            <h1 className="ml-3">Ava Roberts</h1>
+                        </div>
+                        <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
+                            <p className="bg-white h-content p-2">"Apple Pie Cafe was just okay; the pies were decent but nothing exceptional, and the overall experience left much to be desired."</p>
+                        </div>
+                    </div>
+                    <div className="flex mt-16">
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                    </div>
+                </div>
+                {/* Review Card */}
+                <div className="flex flex-col items-center w-[300px] h-[350px] bg-purple-200 border rounded border-[3px] border-black m-4">
+                    <div className="w-11/12  h-1/2 mt-3">
+                        <div className="flex items-center">
+                            <img src={avatar} width="60px" />
+                            <h1 className="ml-3">Benji Anderson</h1>
+                        </div>
+                        <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
+                            <p className="bg-white p-2">"Unfortunately, my experience at Apple Pie Cafe was quite disappointing. The apple pie lacked flavor and had a dry crust, and the overall ambiance felt lackluster and unwelcoming."</p>
+                        </div>
+                    </div>
+                    <div className="flex mt-16">
+                        <img src={starFill} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                        <img src={starBlank} className="w-[40px]" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ReviewModal;
--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -6,6 +6,9 @@ import wAvatar from "../assets/images/woman.png"
 import starBlank from "../assets/images/starBlank.png"
 import starFill from "../assets/images/starFill.png"
 import "../styles/font.css"
+import ReviewModal from "../components/reviewModal"
+import leftArrow from "../assets/images/left-arrow.png"
+import rightArrow from "../assets/images/right-arrow.png"
 
 const LandingPage = () => {
     return (
@@ -25,7 +28,7 @@ const LandingPage = () => {
             </section>
 
             <section>
-                <div className="flex mb-5">
+                <div className="flex ">
                     <div className="w-1/2 h-[500px]">
                         <img src={restInside} className="w-full h-full object-cover" />
                     </div>
@@ -48,90 +51,15 @@ const LandingPage = () => {
 
 
 
-            <section className="mb-12">
+            <section className="mb-12 flex items-center p-[5px] py-4 bg-slate-100">
                 <div>
-                    <h1 className="fontWriting text-4xl">Few Reviews:</h1>
+                    <img src={leftArrow} width="100px"/>
                 </div>
-
-                <div className="flex w-full h-600px bg-slate-100 justify-center">
-
-                    {/* Review Card */}
-                    <div className="flex flex-col items-center w-[300px] h-[350px] bg-blue-200 border rounded border-[3px] border-black m-4">
-                        <div className="w-11/12  h-1/2 mt-3">
-                            <div className="flex items-center">
-                                <img src={avatar} width="64px" />
-                                <h1 className="ml-3">Scott Miguel</h1>
-                            </div>
-                            <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
-                                <p className="bg-white p-2">"I recently had the pleasure of visiting Apple Pie Cafe, and it was an absolute delight. From the moment I stepped in, I was greeted with the warm and inviting aroma of freshly baked pies. The cozy atmosphere, coupled with the friendly and attentive staff, made me feel right at home. The apple pies were simply divine, with flaky crusts that melted in my mouth and fillings bursting with the perfect balance of sweetness and spice. The menu offered a wide variety of flavors and even some unique twists that were a pleasant surprise. The care and attention to detail put into each pie were evident, and it was clear that they were made with love. I left with a happy heart and a newfound appreciation for the art of pie-making. Apple Pie Cafe is a must-visit for any pie lover or anyone looking to experience a slice of Americana in the most delicious way possible."</p>
-                            </div>
-                        </div>
-                        <div className="flex mt-16">
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                        </div>
-                    </div>
-
-                    {/* Review Card */}
-                    <div className="flex flex-col items-center w-[300px] h-[350px] bg-pink-200 border rounded border-[3px] border-black m-4">
-                        <div className="w-11/12  h-1/2 mt-3">
-                            <div className="flex items-center mb-2">
-                                <img src={wAvatar} width="58px" />
-                                <h1 className="ml-3">Olivia Davis</h1>
-                            </div>
-                            <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
-                                <p className="bg-white p-2">"The coffee at Apple Pie Cafe is a delightful complement to their mouthwatering pies. Served piping hot and expertly brewed, it provides a rich and flavorful experience. Whether enjoyed alongside a slice of warm apple pie or on its own, the coffee at Apple Pie Cafe is the perfect way to start your day or unwind in a cozy atmosphere."</p>
-                            </div>
-                        </div>
-                        <div className="flex mt-16">
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                        </div>
-                    </div>
-                    {/* Review Card */}
-                    <div className="flex flex-col items-center w-[300px] h-[350px] bg-green-200 border rounded border-[3px] border-black m-4">
-                        <div className="w-11/12  h-1/2 mt-3">
-                            <div className="flex items-center mb-2">
-                                <img src={wAvatar} width="58px " />
-                                <h1 className="ml-3">Ava Roberts</h1>
-                            </div>
-                            <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
-                                <p className="bg-white h-content p-2">"Apple Pie Cafe was just okay; the pies were decent but nothing exceptional, and the overall experience left much to be desired."</p>
-                            </div>
-                        </div>
-                        <div className="flex mt-16">
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                        </div>
-                    </div>
-                    {/* Review Card */}
-                    <div className="flex flex-col items-center w-[300px] h-[350px] bg-purple-200 border rounded border-[3px] border-black m-4">
-                        <div className="w-11/12  h-1/2 mt-3">
-                            <div className="flex items-center">
-                                <img src={avatar} width="60px" />
-                                <h1 className="ml-3">Benji Anderson</h1>
-                            </div>
-                            <div className="w-full h-full border border-2 border-black overflow-y-scroll " >
-                                <p className="bg-white p-2">"Unfortunately, my experience at Apple Pie Cafe was quite disappointing. The apple pie lacked flavor and had a dry crust, and the overall ambiance felt lackluster and unwelcoming."</p>
-                            </div>
-                        </div>
-                        <div className="flex mt-16">
-                            <img src={starFill} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                            <img src={starBlank} className="w-[40px]" />
-                        </div>
-                    </div>
+                <div>
+                    <ReviewModal />
+                </div>
+                <div>
+                    <img src={rightArrow} width="100px" />
                 </div>
             </section>
 
