7ae9c97e02cc166dce6f9b9ccf1856a0ec558701
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-16 11:46:20 -0700

    updated

--- src/components/menuSidebar.jsx
+++ src/components/menuSidebar.jsx
@@ -11,6 +11,9 @@ const MenuSidebar = () => {
     return (
         <div className="w-content h-full bg-slate-800 flex flex-col justify-top">
             <div className="w-full h-content">
+                <div>
+                    <h1 className="text-white text-2xl p-2 underline py-3 fontWriting">Options:</h1>
+                </div> 
                 {options.map((name, i) => (
                     <div key={i} className="w-full flex flex-col items-center justify-center">
                         <h1 className="w-full text-slate-500 text-2xl cursor-pointer transformation-all duration-200 py-5 px-4 hover:text-slate-100">{name.name}</h1>
