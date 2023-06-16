5e6da0ba34faeb59558ddd20d823805c5613d2d5
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-16 11:21:40 -0700

    updated

--- src/components/menuSidebar.jsx
+++ src/components/menuSidebar.jsx
@@ -2,12 +2,20 @@
 
 const MenuSidebar = () => {
 
-    
+    const options = [
+        { name: "Breakfast" },
+        { name: "Lunch" },
+        { name: "Dessert" }
+    ]
 
     return (
-        <div className="w-[170px] h-full bg-slate-800">
-            <div>
-
+        <div className="w-[170px] h-full bg-slate-800 flex flex-col p-2 justify-center">
+            <div className="w-full h-content bg-red-200">
+                {options.map((name, i) => (
+                    <div key={i}>
+                        <h1 className="text-white text-2xl p-4">{name.name}</h1>
+                    </div>
+                ))}
             </div>
         </div>
     )
