c7956948a7dadd6f931c2aa3b6a72ff86a6e7113
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-16 11:00:40 -0700

    updated

--- src/pages/menuPage.jsx
+++ src/pages/menuPage.jsx
@@ -1,9 +1,16 @@
-
+import fruits from "../assets/images/fruits1.jpeg"
 
 const MenuPage = () => {
-    return(
+    return (
         <div>
-            <h1>Menu</h1>
+            <div className="relative flex w-full justify-center items-center h-[200px] mb-10 mt-5">
+                <h1 className="fontWriting text-7xl font-bold ">Menu</h1>
+                <img className="w-full h-full object-cover absolute z-[-1]" src={fruits} />
+            </div>
+
+            <div className="w-full bg-slate-200 h-[600px]">
+
+            </div>
         </div>
     )
 }
