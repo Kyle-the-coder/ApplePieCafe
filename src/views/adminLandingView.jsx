4bcadc58be28d1ffe4a95b4b4972fafcd5312838
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-19 11:57:22 -0700

    updated

--- src/views/adminLandingView.jsx
+++ src/views/adminLandingView.jsx
@@ -1,10 +1,23 @@
 
 
 const AdminLandingView = () => {
-    return(
+    return (
         <div>
             <h1>welcome to admin page</h1>
+
             <h1 className="underline text-sky-600">logout</h1>
+
+            <div className="w-full bg-slate-200 flex justify-center">
+                <div className="w-[400px] flex flex-col bg-slate-800 text-white px-4 py-6">
+                    <form className="flex-col flex">
+                        <h1 className="fontWriting">Add a Menu Item</h1>
+                        <label>Menu Item Name:</label>
+                        <input type="text" />
+                        <label>Menu Item Description:</label>
+                        <input type="text" />
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
