88e70fcd26f0d0a0d3d7dd77732588b452e54fbb
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-20 12:04:48 -0700

    updated

--- src/views/adminLandingView.jsx
+++ src/views/adminLandingView.jsx
@@ -11,7 +11,32 @@ const AdminLandingView = () => {
     useEffect(() => {
         const uploadMenuItemImg = () => {
             const name = new Date().getTime() + menuItemImg.name
-            const itemImgRef = ref(storage, "menuItemImgs")
+            const itemImgRef = ref(storage, $`menuItemImgs/{name}`)
+
+            const uploadTask = uploadBytesResumable(itemImgRef, menuItemImg);
+
+            uploadTask.on('state_changed',
+                (snapshot) => {
+                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    console.log('Upload is ' + progress + '% done');
+                    switch (snapshot.state) {
+                        case 'paused':
+                            console.log('Upload is paused');
+                            break;
+                        case 'running':
+                            console.log('Upload is running');
+                            break;
+                    }
+                },
+                (error) => {
+                    console.log(error)
+                },
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        console.log('File available at', downloadURL);
+                    });
+                }
+            );
         }
         menuItemImg && uploadMenuItemImg();
     }, [menuItemImg])
