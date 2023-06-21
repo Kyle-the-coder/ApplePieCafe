beb518c87991ce3c87c14af45f0d7c61479d678f
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-21 12:00:06 -0700

    updatedq

---
+++ src/components/dessertMenuItems.jsx
@@ -0,0 +1,11 @@
+
+
+const DessertMenuItems = () => {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default DessertMenuItems;
--- src/views/adminLandingView.jsx
+++ src/views/adminLandingView.jsx
@@ -1,59 +1,7 @@
-import { useEffect, useState } from "react";
-import { db, storage } from "../config/firebase"
-import { doc, addDoc, collection } from "firebase/firestore"
-import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage"
 import BreakfastMenuItem from "../components/breakfastMenuItem";
-
+import LunchMenuItems from "../components/lunchMenuItems";
 const AdminLandingView = () => {
-    const [menuItemName, setMenuItemName] = useState("")
-    const [menuItemDesc, setMenuItemDesc] = useState("")
-    const [menuItemImg, setMenuItemImg] = useState(null)
-    const [menuItemImgRef, setMenuItemImgRef] = useState("")
 
-    useEffect(() => {
-        const uploadMenuItemImg = () => {
-            const name = new Date().getTime() + menuItemImg.name
-            const itemImgRef = ref(storage, `menuItemImgs/${name}` )
-
-            const uploadTask = uploadBytesResumable(itemImgRef, menuItemImg);
-
-            uploadTask.on('state_changed',
-                (snapshot) => {
-                    const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-                    console.log('Upload is ' + progress + '% done');
-                    switch (snapshot.state) {
-                        case 'paused':
-                            console.log('Upload is paused');
-                            break;
-                        case 'running':
-                            console.log('Upload is running');
-                            break;
-                    }
-                },
-                (error) => {
-                    console.log(error)
-                },
-                () => {
-                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                        setMenuItemImgRef(downloadURL)
-                    });
-                }
-            );
-        }
-        menuItemImg && uploadMenuItemImg();
-        
-    }, [menuItemImg])
-
-    const handleAdd = async (e) => {
-        e.preventDefault()
-        // Add a new document in collection "cities"
-        await addDoc(collection(db, "breakfastMenuItems"), {
-            menuItemName: menuItemName,
-            menuItemDescription: menuItemDesc,
-            menuItemImg: menuItemImgRef,
-        });
-    }
-    
 
     return (
         <div>
@@ -62,8 +10,9 @@ const AdminLandingView = () => {
             <h1 className="underline text-sky-600">logout</h1>
 
 
-            <div>
-                <BreakfastMenuItem/>
+            <div className="w-full flex justify-center justify-evenly">
+                <BreakfastMenuItem />
+                <LunchMenuItems />
             </div>
         </div>
     )
