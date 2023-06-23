6341efec151947822124d71ed2a730c5ccf8322e
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-23 12:46:37 -0700

    updated

--- src/components/breakfastModal.jsx
+++ src/components/breakfastModal.jsx
@@ -3,6 +3,7 @@ import { doc, getDocs, collection, arrayRemove } from "firebase/firestore"
 import { db } from "../config/firebase"
 import leftArrow from "../assets/images/modalArrowLeft.png"
 import rightArrow from "../assets/images/modalArrowRight.png"
+import { Transition } from '@headlessui/react';
 
 
 const BreakfastCarousel = () => {
@@ -40,30 +41,43 @@ const BreakfastCarousel = () => {
     const prevSet = () => {
         const newIndex = currentIndex - 1;
         const newSet = breakfastData.slice(newIndex, newIndex + 3);
-        if(currentIndex < 0){
+        if (currentIndex < 0) {
             setCurrentIndex(0)
         } else {
             setActiveSet(newSet);
             setCurrentIndex(newIndex);
-            
+
         }
     };
 
     const isPrevButtonDisabled = currentIndex === 0;
     const isNextButtonDisabled = currentIndex + 3 >= breakfastData.length;
 
-    
+
     console.log(currentIndex)
     return (
         <div className="flex w-full h-full justify-evenly items-center">
             <button src={leftArrow} className={`${isPrevButtonDisabled ? "opacity-50" : "opacity-100"}`} onClick={prevSet} disabled={isPrevButtonDisabled}>
-                <img className="h-[50px] w-[50px]" src={leftArrow}/>
+                <img className="h-[50px] w-[50px]" src={leftArrow} />
             </button>
-            {activeSet.map((picture, index) => (
-                <img key={index} src={picture.menuItemImg} className={`   w-[200px] h-full object-cover opacity-70 hover:opacity-100 cursor-pointer`} alt={`Picture ${index}`} />
-            ))}
+            <Transition
+                as="div"
+                className="flex space-x-4 h-full"
+                show={activeSet.length > 0}
+                enter="transition-all duration-700"
+                enterFrom="-translate-x-full"
+                enterTo="translate-x-0"
+                leave="transition-all duration-700"
+                leaveFrom="translate-x-0"
+                leaveTo="translate-x-full"
+            >
+                {activeSet.map((picture, index) => (
+                    <img key={index} src={picture.menuItemImg} className={`   w-[200px] h-full object-cover opacity-70 hover:opacity-100 cursor-pointer`} alt={`Picture ${index}`} />
+                ))}
+            </Transition>
+
             <button className={`${isNextButtonDisabled ? "opacity-50" : "opacity-100"}`} src={rightArrow} onClick={nextSet} disabled={isNextButtonDisabled}>
-                <img className={` h-[50px] w-[50px]`} src={rightArrow}/>
+                <img className={` h-[50px] w-[50px]`} src={rightArrow} />
             </button>
         </div>
     )
