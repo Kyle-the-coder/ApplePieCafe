a191607732e47b936091147fb636d948b529bc7b
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-23 11:32:04 -0700

    updated

--- src/components/breakfastModal.jsx
+++ src/components/breakfastModal.jsx
@@ -1,9 +1,34 @@
+import { useEffect, useState } from "react";
+import { doc, getDocs, collection } from "firebase/firestore"
+import { db } from "../config/firebase"
 
 
-const BreakfastCarousel = () =>{
-    return(
+const BreakfastCarousel = () => {
+    const [breakfastData, setBreakfastData] = useState({})
+
+    useEffect(() => {
+        // GET BREAKFAST DATA
+        const getBreakfastData = async () => {
+            try {
+                const querySnapshot = await getDocs(collection(db, "breakfastMenuItems"));
+                const documents = querySnapshot.docs.map((doc) => doc.data());
+                setBreakfastData(documents);
+            } catch (error) {
+                console.log(error);
+            }
+        };
+        getBreakfastData();
+
+    }, [])
+
+    const items = [
+        {item: "bfast1", idx: "0"},
+        {item: "bfast2", idx: "1"},
+        {item: "bfast3", idx: "2"}
+    ]
+    return (
         <div>
-            
+
         </div>
     )
 }
