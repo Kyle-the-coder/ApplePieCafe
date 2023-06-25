b2d6f3c89fa9785cbaa412872b7b118b2b533b38
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-25 12:29:54 -0700

    updated

--- src/components/breakfastCarousel.jsx
+++ src/components/breakfastCarousel.jsx
@@ -3,8 +3,18 @@ import { doc, getDocs, collection, arrayRemove } from "firebase/firestore"
 import { db } from "../config/firebase"
 import leftArrow from "../assets/images/modalArrowLeft.png"
 import rightArrow from "../assets/images/modalArrowRight.png"
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
+
+const CarouselNavigation = ({ prevButton, nextButton }) => {
+    return (
+        <div className="flex justify-between items-center">
+            <div>{prevButton}</div>
+            <div>{nextButton}</div>
+        </div>
+    );
+};
 
 
 const BreakfastCarousel = () => {
@@ -28,6 +38,8 @@ const BreakfastCarousel = () => {
         getBreakfastData();
 
         activeSetTracker && setActiveSet(breakfastData.slice(0, 3))
+
+
 
 
     }, [activeSetTracker])
@@ -54,41 +66,32 @@ const BreakfastCarousel = () => {
     const isPrevButtonDisabled = currentIndex === 0;
     const isNextButtonDisabled = currentIndex + 3 >= breakfastData.length;
 
-    const responsive = {
-        superLargeDesktop: {
-            // the naming can be any, depends on you.
-            breakpoint: { max: 4000, min: 3000 },
-            items: 5
-        },
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 3
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1
-        }
+    const handleSlideChange = (event) => {
+        console.log('Slide changed:', event.item);
     };
 
 
     console.log(currentIndex)
     return (
-        <div className="flex w-full h-full justify-evenly items-center">
-            <Carousel responsive={responsive}>
-                {/* <button src={leftArrow} className={`${isPrevButtonDisabled ? "opacity-50" : "opacity-100"}`} onClick={prevSet} disabled={isPrevButtonDisabled}>
-                <img className="h-[50px] w-[50px]" src={leftArrow} />
-            </button> */}
-                {activeSet.map((picture, index) => (
-                    <img key={index} src={picture.menuItemImg} className={`   w-[200px] h-full object-cover opacity-70 hover:opacity-100 cursor-pointer transition-all duration-500`} alt={`Picture ${index}`} />
-                ))}
-                {/* <button className={`${isNextButtonDisabled ? "opacity-50" : "opacity-100"}`} src={rightArrow} onClick={nextSet} disabled={isNextButtonDisabled}>
-                <img className={` h-[50px] w-[50px]`} src={rightArrow} />
-            </button> */}
-            </Carousel>
+        <div className="flex w-full h-full justify-evenly items-center bg-slate-200">
+            <div className="w-full h-full bg-red-200 ">
+
+                <AliceCarousel
+                    items={activeSet.map((picture, index) => (
+                        <img
+                            key={index}
+                            src={picture.menuItemImg}
+                            className="w-[200px] h-auto object-cover opacity-70 hover:opacity-100 cursor-pointer transition-all duration-500"
+                        />
+                    ))}
+                    onSlideChanged={handleSlideChange}
+                    disableDotsControls
+                    
+                />
+            </div>
+
+
+
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthContextProvider } from './context/AuthContext';
+import 'react-alice-carousel/lib/alice-carousel.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
