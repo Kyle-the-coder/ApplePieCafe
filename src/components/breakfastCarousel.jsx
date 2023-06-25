c0a48a9ecca3c84acaefd57de11d18f20df6ba92
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-25 11:13:41 -0700

    updated

--- src/components/breakfastCarousel.jsx
+++ src/components/breakfastCarousel.jsx
@@ -3,8 +3,8 @@ import { doc, getDocs, collection, arrayRemove } from "firebase/firestore"
 import { db } from "../config/firebase"
 import leftArrow from "../assets/images/modalArrowLeft.png"
 import rightArrow from "../assets/images/modalArrowRight.png"
-import { Transition } from '@headlessui/react';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
 
 
 const BreakfastCarousel = () => {
@@ -54,19 +54,41 @@ const BreakfastCarousel = () => {
     const isPrevButtonDisabled = currentIndex === 0;
     const isNextButtonDisabled = currentIndex + 3 >= breakfastData.length;
 
+    const responsive = {
+        superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 5
+        },
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 3
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1
+        }
+    };
+
 
     console.log(currentIndex)
     return (
         <div className="flex w-full h-full justify-evenly items-center">
-            <button src={leftArrow} className={`${isPrevButtonDisabled ? "opacity-50" : "opacity-100"}`} onClick={prevSet} disabled={isPrevButtonDisabled}>
+            <Carousel responsive={responsive}>
+                {/* <button src={leftArrow} className={`${isPrevButtonDisabled ? "opacity-50" : "opacity-100"}`} onClick={prevSet} disabled={isPrevButtonDisabled}>
                 <img className="h-[50px] w-[50px]" src={leftArrow} />
-            </button>
+            </button> */}
                 {activeSet.map((picture, index) => (
                     <img key={index} src={picture.menuItemImg} className={`   w-[200px] h-full object-cover opacity-70 hover:opacity-100 cursor-pointer transition-all duration-500`} alt={`Picture ${index}`} />
                 ))}
-            <button className={`${isNextButtonDisabled ? "opacity-50" : "opacity-100"}`} src={rightArrow} onClick={nextSet} disabled={isNextButtonDisabled}>
+                {/* <button className={`${isNextButtonDisabled ? "opacity-50" : "opacity-100"}`} src={rightArrow} onClick={nextSet} disabled={isNextButtonDisabled}>
                 <img className={` h-[50px] w-[50px]`} src={rightArrow} />
-            </button>
+            </button> */}
+            </Carousel>
         </div>
     )
 }
