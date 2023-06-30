062c896535cf1307f481b8dd7d5639ca9619f063
Kyle Mitchell <kylethacoder@gmail.com>
2023-06-30 12:05:53 -0700

    updated

--- src/pages/landingPage.jsx
+++ src/pages/landingPage.jsx
@@ -74,10 +74,10 @@ const LandingPage = () => {
             <section>
                 <div className="py-5">
                     <h1 className="fontWriting text-3xl mb-3">Recently visited?</h1>
-                    <button className="px-3 py-1 bg-red-300 rounded" onClick={()=>handleReviewModal()}><a href="#reviewStart">Leave a review!</a></button>
+                    <button className="px-3 py-1 bg-red-300 rounded" onClick={() => handleReviewModal()}><a href="#reviewStart">Leave a review!</a></button>
                 </div>
                 <div>
-                    {reviewModalTracker && <ReviewModal  setReviewModalTracker={setReviewModalTracker} handleReviewModal={handleReviewModal}/>}
+                    {reviewModalTracker && <ReviewModal setReviewModalTracker={setReviewModalTracker} handleReviewModal={handleReviewModal} />}
                 </div>
             </section>
 
