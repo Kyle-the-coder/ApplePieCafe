1584f66436a8a84019f715c77e17d92eaab2a969
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-06 12:10:52 -0700

    working out svg

--- src/components/reviewStats.jsx
+++ src/components/reviewStats.jsx
@@ -38,14 +38,31 @@ const ReviewStats = () => {
     return (
         <div className="reviewAverageCardContainer">
             <div className="reviewAverageCard">
+
+                {/* REVIEW STATS TOP */}
                 <div className="reviewAverageCardTop">
+
+                    {/* REVIEW STATS NUMBER */}
                     <div className="reviewAverageCardNumberContainer">
                         <div className="reviewAverageCardNumberOuterCircle">
                             <div className="reviewAverageCardNumberInnerCircle">
                                 <h1 className="fontWriting">{reviewAverage}</h1>
                             </div>
                         </div>
+
+                        <svg xmlns="http://www.w3.org/2000/svg" version="1.1"
+                            width="100%" height="100%">
+                            <defs>
+                                <linearGradient id="gradientColor" >
+                                    <stop offest="0%" stopColor="#e91e63" />
+                                    <stop offset="100%" stopColor="#673ab7" />
+                                </linearGradient>
+                            </defs>
+                            <circle cx="130" cy="120"  rx="50" ry="50" strokeLinecap="round" />
+                        </svg>
                     </div>
+
+                    {/* REVIEW STATS DISPLAY */}
                     <div className="reviewAverageCardStatsContainer">
                         <div class="average-card">
                             <div class="circle"></div>
@@ -53,7 +70,10 @@ const ReviewStats = () => {
                         </div>
                         <h1>goodbye</h1>
                     </div>
+
                 </div>
+
+
 
             </div>
 
