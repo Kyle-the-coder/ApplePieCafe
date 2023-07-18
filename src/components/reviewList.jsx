1bf5575fc4ff8f665933d12a4174d8bc56ddeb32
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-18 11:23:21 -0700

    updated

--- src/components/reviewList.jsx
+++ src/components/reviewList.jsx
@@ -6,7 +6,10 @@ const ReviewList = () => {
         <div className="reviewListBorder">
         <div className="reviewListDataContainer darkBg">
         <div className="reviewListDataTop beigeBg">
-            <h1></h1>
+            <h1 className="fontWriting text-2xl">All Reviews</h1>
+        </div>
+        <div className="reviewListDataDisplay">
+            <h1>list</h1>
         </div>
         </div>
         </div>
