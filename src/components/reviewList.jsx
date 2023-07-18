473bf1e76e626a5dcf7ba7f3454b9abeae08b2e4
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-18 11:19:36 -0700

    updated with list component

---
+++ src/components/reviewList.jsx
@@ -0,0 +1,17 @@
+import "../styles/reviewList.css"
+
+const ReviewList = () => {
+    return(
+        <div className="reviewListContainer">
+        <div className="reviewListBorder">
+        <div className="reviewListDataContainer darkBg">
+        <div className="reviewListDataTop beigeBg">
+            <h1></h1>
+        </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default ReviewList;
