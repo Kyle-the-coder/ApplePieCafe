598d7bf4955aeddabbae3945947e8a2354194e79
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-18 11:39:28 -0700

    updating css

--- src/components/reviewList.jsx
+++ src/components/reviewList.jsx
@@ -1,18 +1,25 @@
+import { useState, useEffect } from "react"
+import { getDocs, collection } from "firebase/firestore";
+import { db } from "../config/firebase";
 import "../styles/reviewList.css"
 
-const ReviewList = () => {
-    return(
+const ReviewList = ({ reviewData }) => {
+    return (
         <div className="reviewListContainer">
-        <div className="reviewListBorder">
-        <div className="reviewListDataContainer darkBg">
-        <div className="reviewListDataTop beigeBg">
-            <h1 className="fontWriting text-2xl">All Reviews</h1>
-        </div>
-        <div className="reviewListDataDisplay darkRedBg">
-            <h1>list</h1>
-        </div>
-        </div>
-        </div>
+            <div className="reviewListBorder">
+                <div className="reviewListDataContainer darkBg">
+                    <div className="reviewListDataTop beigeBg">
+                        <h1 className="fontWriting text-3xl font-bold">All Reviews</h1>
+                    </div>
+                    <div className="reviewListDataDisplay darkRedBg">
+                        {reviewData.map((data, index) => (
+                            <div className="reviewDataSingleContainer darkBg" key={index}>
+                                <img src={data.reviewAvatarImg} className="w-[100px] h-[100px]" />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
