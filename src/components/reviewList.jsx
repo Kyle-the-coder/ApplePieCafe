d7611184cdaa58b80c3ce1f65dc83e8eeabbadf5
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-18 12:06:15 -0700

    updated

--- src/components/reviewList.jsx
+++ src/components/reviewList.jsx
@@ -1,20 +1,31 @@
-import { useState, useEffect } from "react"
-import { getDocs, collection } from "firebase/firestore";
-import { db } from "../config/firebase";
 import "../styles/reviewList.css"
+import blank from "../assets/images/whiteStar.png"
+import fill from "../assets/images/starFill.png"
+import dropDownIcon from "../assets/images/down.png"
 
-const ReviewList = ({ reviewData }) => {
+const ReviewList = ({ reviewData, reviewDataTracker }) => {
     return (
         <div className="reviewListContainer">
             <div className="reviewListBorder">
                 <div className="reviewListDataContainer darkBg">
                     <div className="reviewListDataTop beigeBg">
                         <h1 className="fontWriting text-3xl font-bold">All Reviews</h1>
+                        <img src={dropDownIcon} className="w-[30px] h-[30px]" />
                     </div>
                     <div className="reviewListDataDisplay darkRedBg">
-                        {reviewData.map((data, index) => (
+                        {reviewDataTracker && reviewData.map((data, index) => (
                             <div className="reviewDataSingleContainer darkBg" key={index}>
-                                <img src={data.reviewAvatarImg} className="w-[100px] h-[100px]" />
+                                <img src={data.reviewAvatarImg} className="reviewListDataImg" />
+                                <div className="reviewListStarContainer">
+                                    {[1, 2, 3, 4, 5].map((rating) => (
+                                        <img
+                                            src={rating <= data.reviewInfoRating ? fill : blank}
+                                            className="w-[50px] h-[50px]"
+                                            key={rating}
+                                            alt={`Rating ${rating}`}
+                                        />
+                                    ))}
+                                </div>
                             </div>
                         ))}
                     </div>
