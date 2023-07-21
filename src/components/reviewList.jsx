4160f556536465249dca82493d5363c70c871879
Kyle Mitchell <kylethacoder@gmail.com>
2023-07-21 12:05:25 -0700

    updating sort

--- src/components/reviewList.jsx
+++ src/components/reviewList.jsx
@@ -7,7 +7,13 @@ import dropDownIcon from "../assets/images/chevron.png"
 const ReviewList = ({ reviewData, reviewDataTracker }) => {
     const [isDropdownDisplayed, setIsDropdownDisplayed] = useState(false)
     const [isActive, setIsActive] = useState("")
+    const [selectedSortOption, setSelectedSortOption] = useState([]);
+    const [isSelectedSortDisplayed, setIsSelectedSortDisplayed] = useState(false)
 
+    useEffect(() => {
+        setSelectedSortOption(reviewData)
+        setIsSelectedSortDisplayed(true)
+    }, [])
 
     const displayDropdownToggle = () => {
         if (isDropdownDisplayed) {
@@ -33,31 +39,61 @@ const ReviewList = ({ reviewData, reviewDataTracker }) => {
         }
     }
 
-    const infoRatingSort = reviewDataTracker && reviewData.sort((a, b) => {
+    const handleDropdownSort = (optionName) => {
+        if (selectedSortOption === optionName) {
+            setSelectedSortOption(null); // Deselect the option if it's already selected
+        } else {
+            setSelectedSortOption(optionName); // Set the selected option
+        }
+    }
+
+    const infoRatingSortMostToLeast = reviewDataTracker && reviewData.sort((a, b) => {
         const dateA = a.reviewInfoRating;
         const dateB = b.reviewInfoRating;
         return dateB - dateA;
     });
+    const infoRatingSortLeastToMost = reviewDataTracker && reviewData.sort((a, b) => {
+        const dateA = a.reviewInfoRating;
+        const dateB = b.reviewInfoRating;
+        return dateA - dateB;
+    });
 
+    const sortListContent = [
+        {
+            name: "Rating Most->Least",
+            function: infoRatingSortMostToLeast
+        },
+        {
+            name: "Rating Least->Most",
+            function: infoRatingSortLeastToMost
+        }
+    ]
+
+    console.log(selectedSortOption)
     return (
         <div className="reviewListContainer">
             <div className="reviewListBorder">
                 <div className="reviewListDataContainer bg-white">
                     <div className="reviewListDataTop darkBg">
                         <h1 className="fontWriting text-3xl text-white font-bold">All Reviews</h1>
-                        <div className={`reviewListDataDropdownContainer`} onClick={() => { displayDropdownToggle(); handleDropdownActive() }}>
-                            <img src={dropDownIcon} className="w-[25px] h-[25px]" />
+                        <div className={`reviewListDataDropdownContainer`} >
+                            <img src={dropDownIcon} className="w-[25px] h-[25px]" onClick={() => { displayDropdownToggle(); handleDropdownActive() }} />
                             {isDropdownDisplayed &&
                                 <div className={`reviewListDataDropdownMenu ${isDropdownDisplayed ? isActive : ""}`}>
                                     <div className="reviewListDataDropdownContentContainer">
                                         <h1>Sort List:</h1>
+                                        {sortListContent.map((option, index) => (
+                                            <div key={index} onClick={() => handleDropdownSort(option.function)}>
+                                                <h1>{option.name}</h1>
+                                            </div>
+                                        ))}
                                     </div>
                                 </div>
                             }
                         </div>
                     </div>
                     <div className="reviewListDataDisplay">
-                        {reviewDataTracker && reviewData.map((data, index) => (
+                        {isSelectedSortDisplayed && selectedSortOption.map((data, index) => (
                             <div className="reviewDataSingleContainer" key={index}>
                                 <img src={data.reviewAvatarImg} className="reviewListDataImg" />
                                 <div className="reviewListStarContainer">
