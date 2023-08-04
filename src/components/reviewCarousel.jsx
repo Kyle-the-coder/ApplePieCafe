3336b79be5361980fd01ecc8e6d81c557196ee7d
Kyle Mitchell <kylethacoder@gmail.com>
2023-08-04 10:42:13 -0700

    updated next button functionality

--- src/components/reviewCarousel.jsx
+++ src/components/reviewCarousel.jsx
@@ -58,7 +58,7 @@ const ReviewCarousel2 = ({ reviewModalTracker, reviewData, reviewDataTracker }) 
     //HANDLE BUTTON DISABLE
     const isPrevButtonDisabled = currentIndex === 0;
     const isNextButtonDisabled = currentIndex >= 8;
-    console.log(currentIndex)
+
     return (
         <div className="w-full flex justify-center flex-col ">
 
